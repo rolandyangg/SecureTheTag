84bc08cd02f6579ab449fd847476aec0ab71c8de
whyroland <42717361+whyroland@users.noreply.github.com>
2021-03-08 18:47:02 -0500

    Fix for google cloud

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const upload = multer({
 const app = express();
 
 app.set('view engine', 'ejs');
-const port = 8080;
+const port = 80;
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
